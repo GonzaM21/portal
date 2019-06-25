70d629e24613a0b035b6fb8cd4b5775ce6bfe963
jowisaidmajn <joelsaidman1@gmail.com>
2019-06-25 15:18:06 -0300

    agrego instalador

---
+++ install.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+echo "Beggin Portal installer..."
+echo "
+  _______         ̣_____               
+ |  ___  |       / __  \_________  ___     _
+ | |___| | ______||__| |__    __| / _ \   | |
+ |  _____||  __  |     /  |  |   / /_\ \  | |
+ | |      | |__| | |\ \   |  |  / /   \ \ | |___
+ |_|      |______|_| \_\  |__| /_/     \_\|_____|
+ 
+ "
+echo "Installing cmake..."
+sudo apt-get install cmake
+echo "Installing gcc..."
+sudo apt-get install gcc
+echo "Updating..."
+sudo apt-get update && sudo apt-get install build-essential
+echo "Installing sdl2 libs..."
+sudo apt-get install libsdl2-dev
+sudo apt-get install libsdl2-image-dev
+sudo apt-get install qt5-default
+sudo apt-get install libsdl2-mixer-dev
+echo "Installing qt5..."
+sudo apt-get install qt5-default
+echo "Installing ffmpeg..."
+sudo apt-get install ffmpeg
+echo "Decompressing images and sounds..."
+sudo rm -rf resources
+mkdir resources
+unzip resources.zip
+unzip qt.zip
+unzip sounds.zip
+unzip img.zip
+sudo rm -rf qt.zip
+sudo rm -rf sounds.zip
+sudo rm -rf img.zip
+sudo mv qt resources
+sudo mv Music resources
+sudo mv img resources 
+echo "Building executers..."
+cmake CMakeLists.txt
+make -j128
+echo "Deleting unecesary folders..."
+sudo rm -rf CMakeFiles
+sudo rm -rf CMakeCache.txt
+sudo rm -rf Makefile
+sudo rm -rf cmake_install.cmake
+sudo rm -rf resources.zip
+echo "Installation finish. Good game!"
